c71ed53a1a1b574fd4167252bd56aecd38e95f4f
Justine <40345340+ohjust@users.noreply.github.com>
2021-02-04 08:39:47 -0800

    src files
    added html, js and css files

---
+++ script.js
@@ -0,0 +1,52 @@
+var toDoEntryBox = document.getElementById("todo-entry-box");
+var toDoList = document.getElementById("todo-list");
+
+
+var addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addToDoItem);
+function addToDoItem() {
+    var itemText = toDoEntryBox.value;
+    newToDoItem(itemText, false);
+}
+
+var clearButton = document.getElementById("clear-completed-button");
+clearButton.addEventListener("click", clearCompletedToDoItems);
+function clearCompletedToDoItems() {
+    var completedItems = toDoList.getElementsByClassName("completed");
+
+    while (completedItems.length > 0){
+        completedItems.item(0).remove();
+    }
+}
+
+var emptyButton = document.getElementById("empty-button");
+emptyButton.addEventListener("click", emptyList);
+function emptyList() {
+    var toDoItems = toDoList.children;
+    while (toDoItems.length > 0){
+        toDoItems.item(0).remove();
+    }
+}
+
+
+function newToDoItem(itemText, completed){
+    var toDoItem = document.createElement("li");
+    var toDoText = document.createTextNode(itemText);
+    toDoItem.appendChild(toDoText);
+
+    if (completed) {
+        toDoItem.classList.add("completed");
+    }
+
+    toDoList.appendChild(toDoItem);
+    toDoItem.addEventListener("click", toggleToDoItemState);
+}
+
+function toggleToDoItemState(){
+    if (this.classList.contains("completed")){
+        this.classList.remove("completed");
+    } else {
+        this.classList.add("completed");
+    }
+}
+
